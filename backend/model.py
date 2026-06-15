47bd1539ebfa78ac34441a2ff340d1cad26e5f99
Swastik Kulkarni <swastikkulkarni737@gmail.com>
2026-06-15 13:12:41 +0530

    Fix: the API call from backend

--- backend/model.py
+++ backend/model.py
@@ -50,3 +50,19 @@ class ShopifyStore(db.Model):
         db.Text,
         nullable=False
     )
+
+class AppSetting(db.Model):
+    __tablename__ = "app_settings"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    key = db.Column(
+        db.String(255),
+        unique=True,
+        nullable=False
+    )
+
+    value = db.Column(
+        db.Text,
+        nullable=False
+    )
