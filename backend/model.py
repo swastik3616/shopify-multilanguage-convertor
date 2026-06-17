91cfc1dea71f0c561e9b6f3be356116c85013f79
Swastik Kulkarni <swastikkulkarni737@gmail.com>
2026-06-17 11:07:43 +0530

    Remove installation_date column

--- backend/model.py
+++ backend/model.py
@@ -4,85 +4,81 @@ from datetime import datetime
 class Translation(db.Model):
     __tablename__ = "translations"
 
-    id = db.Column(db.Integer, primary_key=True)
+id = db.Column(db.Integer, primary_key=True)
 
-    source_text = db.Column(
-        db.Text,
-        nullable=False
-    )
+source_text = db.Column(
+    db.Text,
+    nullable=False
+)
 
-    target_language = db.Column(
-        db.String(100),
-        nullable=False
-    )
+target_language = db.Column(
+    db.String(100),
+    nullable=False
+)
 
-    translated_text = db.Column(
-        db.Text,
-        nullable=False
-    )
+translated_text = db.Column(
+    db.Text,
+    nullable=False
+)
 
 class PageContent(db.Model):
     __tablename__ = "page_contents"
 
-    id = db.Column(db.Integer, primary_key=True)
-    page = db.Column(db.String(255), nullable=False)
-    key = db.Column(db.String(255), nullable=False)
-    source_text = db.Column(db.Text, nullable=False)
+id = db.Column(db.Integer, primary_key=True)
+page = db.Column(db.String(255), nullable=False)
+key = db.Column(db.String(255), nullable=False)
+source_text = db.Column(db.Text, nullable=False)
 
-    __table_args__ = (
-        db.UniqueConstraint('page', 'key', name='uq_page_key'),
-    )
+__table_args__ = (
+    db.UniqueConstraint('page', 'key', name='uq_page_key'),
+)
 
 class AuditLog(db.Model):
     __tablename__ = "audit_logs"
 
-    id = db.Column(db.Integer, primary_key=True)
+id = db.Column(db.Integer, primary_key=True)
 
-    action = db.Column(
-        db.String(255),
-        nullable=False
-    )
+action = db.Column(
+    db.String(255),
+    nullable=False
+)
 
-    created_at = db.Column(
-        db.DateTime,
-        default=datetime.utcnow
-    )
-    
+created_at = db.Column(
+    db.DateTime,
+    default=datetime.utcnow
+)
+
 class ShopifyStore(db.Model):
     __tablename__ = "shopify_stores"
 
-    id = db.Column(db.Integer, primary_key=True)
+id = db.Column(db.Integer, primary_key=True)
 
-    shop = db.Column(
-        db.String(255),
-        nullable=False
-    )
+shop = db.Column(
+    db.String(255),
+    nullable=False
+)
 
-    access_token = db.Column(
-        db.Text,
-        nullable=False
-    )
-    installation_date = db.Column(
-        db.DateTime,
-        default=datetime.utcnow
-    )
+access_token = db.Column(
+    db.Text,
+    nullable=False
+)
 
-    __table_args__ = (
-        db.UniqueConstraint('shop', name='uq_shop'),
-    )
+__table_args__ = (
+    db.UniqueConstraint('shop', name='uq_shop'),
+)
 
 class AppSetting(db.Model):
     __tablename__ = "app_settings"
 
-    id = db.Column(db.Integer, primary_key=True)
+id = db.Column(db.Integer, primary_key=True)
 
-    key = db.Column(
-        db.String(255),
-        unique=True,
-        nullable=False
-    )
+key = db.Column(
+    db.String(255),
+    unique=True,
+    nullable=False
+)
 
-    value = db.Column(
-        db.Text,
-        nullable=False
-    )
+value = db.Column(
+    db.Text,
+    nullable=False
+)
